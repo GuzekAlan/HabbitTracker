5c64b7fe75cb810f33bf67aad15f45f1bccf5fa2
Alan Guzek <alan.guzek@swmansion.com>
2024-06-12 21:37:43 +0200

    reconfigure shadcn, add browser router

--- frontend_new/src/App.tsx
+++ frontend_new/src/App.tsx
@@ -1,10 +1,14 @@
-import  {Button} from "@/components/ui/button";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+
 
 function App() {
   return (
-    <>
-      <Button>Habbit Tracker</Button>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
