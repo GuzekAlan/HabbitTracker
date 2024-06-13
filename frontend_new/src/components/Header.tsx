7c0c9f16d5185a003647a4924d0a371c8c7fac54
Alan Guzek <alan.guzek@swmansion.com>
2024-06-13 19:46:27 +0200

    frontend yupii

--- frontend_new/src/components/Header.tsx
+++ frontend_new/src/components/Header.tsx
@@ -1,14 +1,13 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "./shadcn/ui/breadcrumb";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from "./shadcn/ui/breadcrumb";
 
 function Header() {
   return (
-    <div className="w-full flex flex-row justify-center p-2">
-        <Breadcrumb className="border border-foreground px-4 py-2 rounded-xl">
-            <BreadcrumbList className="text-2xl text-primary hover:text-secondary">
+    <div className="w-full p-2 bg-primary">
+        <Breadcrumb>
+            <BreadcrumbList className="space-x-8 text-2xl text-primary-foreground">
                 <BreadcrumbItem>
                     <BreadcrumbLink href="/">Home</BreadcrumbLink>
                 </BreadcrumbItem>
-                <Separator />
                 <BreadcrumbItem>
                     <BreadcrumbLink href="/create_habit">Create Habit</BreadcrumbLink>
                 </BreadcrumbItem>
@@ -16,14 +15,6 @@ function Header() {
         </Breadcrumb>
     </div>
   );
-}
-
-function Separator() {
-    return (
-        <BreadcrumbSeparator className="text-foreground">
-            / 
-        </BreadcrumbSeparator>
-    )
 }
 
 export default Header;
